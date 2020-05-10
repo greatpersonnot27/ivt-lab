23830816c7a2cede4633882110391d68acc97861
Irakli <greatpersonnot27@gmail.com>
2020-05-10 20:40:17 +0200

    Add dependency injection

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,22 +8,29 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  private TorpedoStore mockPrimaryTorpedoStore;
+  private TorpedoStore mockSecondaryTorpedoStore;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPrimaryTorpedoStore = mock(TorpedoStore.class);
+    mockSecondaryTorpedoStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimaryTorpedoStore, mockSecondaryTorpedoStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockPrimaryTorpedoStore.fire(1)).thenReturn(true);
+    when(mockSecondaryTorpedoStore.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(mockPrimaryTorpedoStore,times(1)).fire(1);
+    verify(mockSecondaryTorpedoStore, times(1)).fire(1);
   }
 
   @Test
